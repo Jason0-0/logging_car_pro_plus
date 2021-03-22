041f3288daa5a695231f41d7b5ededd5901da441
剑燊 颜 <yanjianshen1999@live.com>
2021-03-22 17:02:29 +0800

    增加了匀速转动及获取当前角度的功能

--- Core/Bsp/servo.c
+++ Core/Bsp/servo.c
@@ -1,6 +1,9 @@
 #include "servo.h"
 #include "math.h"
 
+#define TIM_FREQ (1e6)
+#define TIM_ARR (20000)
+
 struct Servo_t servo_grab={
     .htim=&htim4,
     .tim_ch=TIM_CHANNEL_1,
@@ -35,4 +38,36 @@ void servo_set_angle(struct Servo_t* servo,uint16_t angle)
     servo->angle=angle;
     __HAL_TIM_SetCompare(servo->htim,servo->tim_ch,new_ccr);
 
+}
+
+//考虑做成线程？
+void servo_step_run(struct Servo_t* servo,uint16_t set_angle,int speed)
+{
+    //越界处理...
+
+    static uint8_t is_CW;
+    is_CW=set_angle-servo->angle>0?1:0;
+
+    if (is_CW)
+    {
+        for(uint16_t i=servo->angle;i<=set_angle;i++)
+        {
+            servo_set_angle(servo,i);
+            HAL_Delay(speed);   //os_Delay(speed);
+        }
+    }
+    else
+    {
+        for(uint16_t i=servo->angle;i>=set_angle;i--)
+        {
+            servo_set_angle(servo,i);
+            HAL_Delay(speed);   //os_Delay(speed);
+        }
+    }
+}
+
+uint16_t servo_get_angle(struct Servo_t* servo)
+{
+    uint16_t ccrx=__HAL_TIM_GetCompare(servo->htim,servo->tim_ch); //ms*1000
+    return (ccrx-500)*servo->max_angle/2000;
 }
